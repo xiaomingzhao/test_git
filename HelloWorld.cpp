134e9ede249c9c8566788befaadc11bbd17a235d
Xiaoming Zhao <xiaoming.zhao@yahoo.com>
2010-11-12 10:44:25 -0600

    first version Signed-off-by: Xiaoming Zhao <xiaoming.zhao@yahoo.com>

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: Nov 12, 2010
+ *      Author: xiaomingzhao
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello World" << endl;
+	return 0;
+}
